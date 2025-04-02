066c6ccb71039a6440449f0cf2f39e266c0428d4
Ali Alam <aali@ichk.edu.hk>
2025-04-02 09:32:19 +0800

    Updated the CHANGEDB.php

--- Professional Development/CHANGEDB.php
+++ Professional Development/CHANGEDB.php
@@ -36,3 +36,8 @@ ALTER TABLE `professionalDevelopmentRequests` CHANGE `coverAmount` `expenseReque
 ALTER TABLE `professionalDevelopmentRequestPerson` ADD `gibbonFinanceExpenseID` INT(14) UNSIGNED ZEROFILL NULL AFTER `role`;end
 INSERT INTO `gibbonNotificationEvent` (`event`, `moduleName`, `actionName`, `type`, `scopes`, `active`) VALUES ('Expense Request Notifications', 'Professional Development', 'New Application_my', 'Additional', 'All', 'Y');end
 ";
+
+// v0.0.03
+$count++;
+$sql[$count][0] = "0.0.03";
+$sql[$count][1] = "";
