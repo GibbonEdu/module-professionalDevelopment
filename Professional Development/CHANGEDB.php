c6eb21faf4142e8f9754cebd8141d88e91babbf6
Ali Alam <aalam@ichk.edu.hk>
2025-08-28 11:16:51 +0800

    PD: Updated the data type to store longer text in Key Takeaways (#22)

--- Professional Development/CHANGEDB.php
+++ Professional Development/CHANGEDB.php
@@ -98,3 +98,10 @@ $sql[$count][0] = "0.0.11";
 $sql[$count][1] = "
 UPDATE `professionalDevelopmentPortfolio` SET `professionalDevelopmentRequestID` = NULL WHERE `professionalDevelopmentRequestID` = 0000000000;end
 ";
+
+// v0.0.12
+$count++;
+$sql[$count][0] = "0.0.12";
+$sql[$count][1] = "
+ALTER TABLE `professionalDevelopmentPortfolio` CHANGE `keyTakeaways` `keyTakeaways` TEXT NOT NULL;end
+";
