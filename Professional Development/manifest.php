c354c10c9065308161a9bc9844c8bef189d783a8
Ali Alam <aalam@ichk.edu.hk>
2026-01-30 09:32:33 +0800

    PD Portfolio: Increased the character limit for title field in PD Portfolio (#28)

--- Professional Development/manifest.php
+++ Professional Development/manifest.php
@@ -27,7 +27,7 @@ $description = 'A Professional Development (PD) module for Gibbon to record Staf
 $entryURL    = "pd_manage.php";   // The landing page for the unit, used in the main menu
 $type        = "Additional";
 $category    = 'Other';
-$version     = '0.1.03';
+$version     = '0.1.04';
 $author      = 'Gibbon Foundation';
 $url         = 'https://github.com/GibbonEdu/module-professionalDevelopment';
 
